6fe518e34855ca0bef75ea2220d0002a545e11ea
Brandao-lang <elijahbrandao@gmail.com>
2021-06-14 15:07:27 -0400

    added a test

--- project.js
+++ project.js
@@ -2,4 +2,8 @@ function getGreeting(name){
     return "Hi " + name;
 }
 
+function addition(num1, num2) {
+    return num1 + num2
+}
+
 module.exports = getGreeting;
--- test.js
+++ test.js
@@ -1,5 +1,17 @@
 const getGreeting = require('./project');
 
+function addition(num1, num2) {
+  return num1 + num2
+}
+
+
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
 });
+
+test('Gives the sum of two numbers', () => {
+  expect(addition(2,2)).toBe(4);
+});
+
+
+
